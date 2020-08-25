cd9015ab71e3b32aade5d31762bcff0becd97901
Sanshain <digital-mag@ya.ru>
2020-08-25 20:46:33 +0300

    empty compile w/o errors on rollup -c

--- init.ts
+++ init.ts
@@ -3,11 +3,11 @@ var config = {
 
 }
 
-/*
+
 import { Ajax } from "./source/ajax";
 import { fragmentRefresh, RefreshManager } from "./source/snippet";
 import { vom, renderPage, Viewer } from "./source/base"; //*/
 
-import { vom } from "./source/base";
+// import { vom } from "./source/base";
 
 
