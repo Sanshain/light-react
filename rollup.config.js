27d17caa0911871b8aa9d637fa5fefaf9a794151
Sanshain <digital-mag@ya.ru>
2020-08-28 12:32:24 +0300

    uglify tunning

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 
 
 import typescript from '@rollup/plugin-typescript';          // https://github.com/rollup/plugins/tree/master/packages/typescript
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: 'init.ts',
@@ -19,6 +20,14 @@ export default {
       lib: ["es6", "dom"], //es5
       target: "es5",
       sourceMap: true
-    }),    
+    }),
+    uglify({
+      mangle: {
+        keep_fnames : true,
+        ie8: true 
+        // toplevel: true,
+        // properties: true        
+      }
+    })//*/
   ]
 }
